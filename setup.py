f7f44d769aba23bab176bc860d5d4e5c4efe2e4f
Chenghao <mouchenghao@gmail.com>
2019-09-22 09:31:54 -0700

    remove support for itunes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="touchbar-lyric",
-    version="0.1.7",
+    version="0.1.8",
     author="Chenghao",
     python_requires='>3.6.0',
     author_email="mouchenghao@gmail.com",
