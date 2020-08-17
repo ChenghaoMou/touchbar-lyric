6f39a00584e49848a138e39bddf21b938e8254c9
Chenghao Mou <mouchenghao@gmail.com>
2020-08-16 17:15:19 -0700

    reformat

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name="touchbar-lyric",
-    version="0.5.1",
+    version="0.5.2",
     author="Chenghao",
     python_requires=">3.7.0",
     author_email="mouchenghao@gmail.com",
